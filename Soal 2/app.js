79cbbc51f0c6b2dd751e0139f76878756b3134f9
Cef Syarif <chefdeveloper29@gmail.com>
2021-03-21 11:16:02 +0700

    Soal2 Done

---
+++ Soal 2/app.js
@@ -0,0 +1,21 @@
+var inputs = []
+var angka = 0
+console.log("asdasd")
+do{
+  angka = prompt('Masukan angka')
+  if(angka.match(/[^\d]+/g)){
+    if(!angka.match(/[-]+/g)){
+      break;
+    }
+  }
+  inputs.push(angka)
+}while(angka.match(/[\d]+/g))
+console.log(inputs)
+
+if(inputs.length == 1){
+  console.log(`Nilai tertinggi : ${inputs[0]}`)
+  console.log(`Nilai terendah : ${inputs[0]}`)
+}else{
+  console.log(`Nilai tertinggi : ${inputs.reduce(function(a, b) { return Math.max(a,b) })}`)
+  console.log(`Nilai terendah : ${inputs.reduce(function(a, b) { return Math.min(a,b) })}`)
+}
