8d6be921c0112bafc71c868d490789d177908d2e
Cef Syarif <chefdeveloper29@gmail.com>
2021-03-21 12:10:04 +0700

    Soal 2 Done v.2

--- Soal 2/app.js
+++ Soal 2/app.js
@@ -9,12 +9,23 @@ do{
   }
   inputs.push(angka)
 }while(angka.match(/[\d]+/g))
-console.log(inputs)
 
 if(inputs.length == 1){
-  console.log(`Nilai tertinggi : ${inputs[0]}`)
-  console.log(`Nilai terendah : ${inputs[0]}`)
-}else{
+  console.log(inputs)
   console.log(`Nilai tertinggi : ${inputs.reduce(function(a, b) { return Math.max(a,b) })}`)
   console.log(`Nilai terendah : ${inputs.reduce(function(a, b) { return Math.min(a,b) })}`)
+}else{
+  let nilaiTertinggi = inputs[0]
+  let nilaiTerendah = inputs[0]
+  for(let i = 1; i < inputs.length;i++){
+    if(parseInt(inputs[i]) > parseInt(nilaiTertinggi)){
+      nilaiTertinggi = inputs[i]
+    }
+    if(parseInt(inputs[i]) < parseInt(nilaiTerendah)){
+      nilaiTerendah = inputs[i]
+    }
+  }
+  console.log(inputs.sort())
+  console.log(`Nilai Tertinggi : ${nilaiTertinggi}`)
+  console.log(`Nilai Terendah : ${nilaiTerendah}`)
 }
